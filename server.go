d036ef940fa09a3760ba76118a935e0b24710dfe
Thanos Theodosiou <thanosthd@gmail.com>
2021-09-11 19:03:07 +0300

    Main: Initilaize routes before starting server

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ const port = "8888";
 
 func main() {
 	listeningPort := fmt.Sprintf(":%s",port);
+	InitializeRoutes();
 	err := http.ListenAndServe(listeningPort, nil);
 	if err != nil {
 		log.Fatalf("Error happened when starting server: %s\n", err.Error());
