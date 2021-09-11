da6d22595c9a78901658ec75d579ea248dd7a7a1
Thanos Theodosiou <thanosthd@gmail.com>
2021-09-11 19:09:02 +0300

    Main: Add log to let user know that the server will start

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ const port = "8888";
 func main() {
 	listeningPort := fmt.Sprintf(":%s",port);
 	InitializeRoutes();
+	log.Printf("Starting server on localhost:%s", port);
 	err := http.ListenAndServe(listeningPort, nil);
 	if err != nil {
 		log.Fatalf("Error happened when starting server: %s\n", err.Error());
