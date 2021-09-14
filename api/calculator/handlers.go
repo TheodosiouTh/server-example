ce9e49b0987868b285c99b2ac7ff55fb5a91068d
Thanos Theodosiou <thanosthd@gmail.com>
2021-09-14 18:25:14 +0300

    Calculator Sum Handler: Fix Sending Get request with body

--- api/calculator/handlers.go
+++ api/calculator/handlers.go
@@ -1,25 +1,27 @@
 package calculator
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-type sumBody struct {
-	Numbers []int
-}
-
 func DoubleHandler(response http.ResponseWriter, request *http.Request) {
-	var body sumBody
-	err := json.NewDecoder(request.Body).Decode(&body)
-	if err != nil {
-		log.Print("Could not decode request body")
-		http.Error(response, err.Error(), http.StatusBadRequest)
-		return
+	sentNumbers := request.URL.Query()["numbers"]
+
+	var numbers []int
+	for _, numberString := range sentNumbers {
+		number, err := strconv.ParseInt(numberString, 10, 64)
+		if err != nil {
+			log.Print("Could not decode request body")
+			http.Error(response, err.Error(), http.StatusBadRequest)
+			fmt.Fprintln(response, "No numbers or special characters allowed.")
+			return
+		}
+		numbers = append(numbers, int(number))
 	}
 
-	result := Sum(body.Numbers)
+	result := Sum(numbers)
 	fmt.Fprintln(response, result)
 }
